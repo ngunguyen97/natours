a240b6fd7bd3ba8d25f3e3fe079752b868330be7
lucynguyen <ngu.nguyen@kyanon.digital>
2022-06-15 10:40:16 +0700

    Chaining multiple middleware function

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,14 +5,23 @@ const tours = JSON.parse(
 );
 
 exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
-
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
     });
   }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.hasOwnProperty('name') || !req.body.hasOwnProperty('price')) {
+    return res.status(400).json({
+      status: 'fail',
+      messsage: 'Missing name or price',
+    });
+  }
+
   next();
 };
 
@@ -42,7 +51,7 @@ exports.createTour = (req, res) => {
 
   tours.push(newTour);
   fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
+    `${__dirname}/../dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (error) => {
       if (error) throw error;
--- routers/tourRouters.js
+++ routers/tourRouters.js
@@ -5,9 +5,14 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
+// Create a checkBody middleware
+// Check if body contains the name and price properties.
+// If not, send back 400 (bad request)
+// Add it to the post hanlder stack.
+
 router.get('/', tourController.getAllTours);
 router.get('/:id', tourController.getTour);
-router.post('/', tourController.createTour);
+router.post('/', tourController.checkBody, tourController.createTour);
 router.patch('/:id', tourController.updateTour);
 router.delete('/:id', tourController.deleteTour);
 
