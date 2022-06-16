16eb12c4e72e941c08ae76b8913d0d37d404d09f
lucynguyen <ngu.nguyen@kyanon.digital>
2022-06-16 18:04:09 +0700

    Added mongoose lib and Connecting Our Database with the Express App

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  //.connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('DB connection successfull!');
+  });
 
 const app = require('./app');
 
