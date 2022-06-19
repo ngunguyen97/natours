34aec681d9d87487b56484856e4e03acc8fe5ba8
lucynguyen <ngu.nguyen@kyanon.digital>
2022-06-19 16:38:50 +0700

    Making the API Better Advanced Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,13 +6,20 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    console.log(req.query, queryObj);
+    // 2) Advanced Filtering
+    const queryStr = JSON.stringify(queryObj).replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      match => `$${match}`
+    );
 
-    const query = Tour.find(queryObj);
+    //console.log(req.query, JSON.parse(queryStr));
+
+    const query = Tour.find(JSON.parse(queryStr));
 
     // const query = Tour.find()
     //   .where('duration')
