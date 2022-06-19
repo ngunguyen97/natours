cf186500a5b07acd2fd19fb02615e4773661bb93
lucynguyen <ngu.nguyen@kyanon.digital>
2022-06-19 17:05:29 +0700

    Making the API Better Sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,12 +6,12 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
-    // 1) Filtering
+    // 1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    // 2) Advanced Filtering
+    // 1B) Advanced Filtering
     const queryStr = JSON.stringify(queryObj).replace(
       /\b(gte|gt|lte|lt)\b/g,
       match => `$${match}`
@@ -19,7 +19,15 @@ exports.getAllTours = async (req, res) => {
 
     //console.log(req.query, JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // const query = Tour.find()
     //   .where('duration')
