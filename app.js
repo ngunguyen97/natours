8dbc4981dd0851d72951fffb5f489cdca2685450
lucynguyen <ngu.nguyen@kyanon.digital>
2022-08-08 18:19:29 +0700

    Extending Our Base Template with Blocks

--- app.js
+++ app.js
@@ -63,7 +63,19 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
   res.status(200).render('base', {
-    tour: 'The Forest Hiker'
+    title: 'The Forest Hiker'
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All tour'
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour'
   });
 });
 
