590d7cedf36e8ad107c0785ca207c94b46da04f4
lucynguyen <ngu.nguyen@kyanon.digital>
2022-08-09 10:53:55 +0700

    Building the Tour Overview

--- routers/viewRoutes.js
+++ routers/viewRoutes.js
@@ -3,13 +3,7 @@ const viewsController = require('./../controllers/viewsController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).render('base', {
-    title: 'All Tours'
-  });
-});
-
-router.get('/overview', viewsController.getOverview);
+router.get('/', viewsController.getOverview);
 
 router.get('/tour', viewsController.getTour);
 
