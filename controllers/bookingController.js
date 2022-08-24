1449f5c10cf996215c492cc8609c97c5c9117c36
lucynguyen <ngu.nguyen@kyanon.digital>
2022-08-24 18:22:43 +0700

    Updated User.find to User.findOne and Image Url

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -24,7 +24,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`]
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`
+            ]
           },
           unit_amount_decimal: tour.price * 100,
           currency: 'usd'
@@ -43,7 +47,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
-  const user = (await User.find({ email: session.customer_email })).id;
+  const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
